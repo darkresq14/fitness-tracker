77b7d49f70ba15a087299100792683b794e7fcc3
dArKrEsQ <razvan.bielz@gmail.com>
2022-10-17 12:07:46 +0300

    Added auth listener for login/logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,21 +2,40 @@ import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
 import { AngularFireAuth } from "angularfire2/auth";
 import { Subject } from "rxjs";
+import { TrainingService } from "../training/training.service";
 import { AuthData } from "./auth-data.model";
-import { User } from "./user.model";
 
 @Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private isAuthenticated = false;
 
-  constructor(private router: Router, private afAuth: AngularFireAuth) {}
+  constructor(
+    private router: Router,
+    private afAuth: AngularFireAuth,
+    private trainingService: TrainingService
+  ) {}
+
+  initAuthListener(): void {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.authChange.next(true);
+        this.router.navigate(["/training"]);
+      } else {
+        this.trainingService.cancelSubscriptions();
+        this.isAuthenticated = false;
+        this.authChange.next(false);
+        this.router.navigate(["/login"]);
+      }
+    });
+  }
 
   registerUser(authData: AuthData) {
     this.afAuth.auth
       .createUserWithEmailAndPassword(authData.email, authData.password)
       .then((res) => {
-        this.authSuccess();
+        console.log(res);
       })
       .catch((err) => console.log(err));
   }
@@ -25,24 +44,22 @@ export class AuthService {
     this.afAuth.auth
       .signInWithEmailAndPassword(authData.email, authData.password)
       .then((res) => {
-        this.authSuccess();
+        console.log(res);
       })
       .catch((err) => console.log(err));
   }
 
   logout() {
-    this.isAuthenticated = false;
-    this.authChange.next(false);
-    this.router.navigate(["/login"]);
+    this.afAuth.auth.signOut();
   }
 
   isAuth() {
     return this.isAuthenticated;
   }
 
-  authSuccess() {
-    this.isAuthenticated = true;
-    this.authChange.next(true);
-    this.router.navigate(["/training"]);
-  }
+  // authSuccess() {
+  //   this.isAuthenticated = true;
+  //   this.authChange.next(true);
+  //   this.router.navigate(["/training"]);
+  // }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { Exercise } from "./exercise.model";
-import { Subject } from "rxjs";
+import { Subject, Subscription } from "rxjs";
 import { AngularFirestore } from "angularfire2/firestore";
 import "rxjs/add/operator/map";
 
@@ -15,37 +15,43 @@ export class TrainingService {
   availableExercisesChanged = new Subject<Exercise[]>();
   pastExercisesChanged = new Subject<Exercise[]>();
 
+  private fbSubs: Subscription[] = [];
+
   constructor(private db: AngularFirestore) {}
 
   fetchAvailableExercises() {
-    this.db
-      .collection("availableExercises")
-      .snapshotChanges()
-      .map((docArray) =>
-        docArray.map((doc) => {
-          return {
-            id: doc.payload.doc.id,
-            ...(doc.payload.doc.data() as Exercise),
-          };
+    this.fbSubs.push(
+      this.db
+        .collection("availableExercises")
+        .snapshotChanges()
+        .map((docArray) =>
+          docArray.map((doc) => {
+            return {
+              id: doc.payload.doc.id,
+              ...(doc.payload.doc.data() as Exercise),
+            };
+          })
+        )
+        .subscribe((res) => {
+          this.availableExercises = res;
+          this.availableExercisesChanged.next([...this.availableExercises]);
         })
-      )
-      .subscribe((res) => {
-        this.availableExercises = res;
-        this.availableExercisesChanged.next([...this.availableExercises]);
-      });
+    );
+  }
+
+  fetchPastExercises() {
+    this.fbSubs.push(
+      this.db
+        .collection("pastExercises")
+        .valueChanges()
+        .subscribe((exercises: Exercise[]) => {
+          this.pastExercisesChanged.next(exercises);
+        })
+    );
   }
 
   getRunningExercise() {
     return { ...this.runningExercise };
-  }
-
-  fetchPastExercises() {
-    this.db
-      .collection("pastExercises")
-      .valueChanges()
-      .subscribe((exercises: Exercise[]) => {
-        this.pastExercisesChanged.next(exercises);
-      });
   }
 
   startExercise(selectedId: string) {
@@ -75,6 +81,10 @@ export class TrainingService {
     });
     this.runningExercise = null;
     this.runningExerciseChanged.next(null);
+  }
+
+  cancelSubscriptions() {
+    this.fbSubs.forEach((sub) => sub.unsubscribe());
   }
 
   private addDataToDatabase(exercise: Exercise) {
