4e98e1d0763ead575e45af4226bec85f3207d671
dArKrEsQ <razvan.bielz@gmail.com>
2022-10-17 15:57:35 +0300

    fixed StopComponent and prod env

--- src/app/training/current-training/stop-training.component.ts
+++ src/app/training/current-training/stop-training.component.ts
@@ -3,19 +3,10 @@ import { MAT_DIALOG_DATA } from "@angular/material";
 
 @Component({
   selector: "app-stop-training",
-  template: `
-    <h1 mat-dialog-title>Are you sure?</h1>
-    <mat-dialog-content>
-      <p>You already got {{ passedData.progress }}%</p>
-    </mat-dialog-content>
-    <mat-dialog-actions>
-      <button mat-button [mat-dialog-close]="true">Yes</button>
-      <button mat-button [mat-dialog-close]="false">No</button>
-    </mat-dialog-actions>
-  `,
+  templateUrl: "./stop-training.component.html",
 })
 export class StopTrainingComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) private passedData) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData) {}
 
   ngOnInit() {}
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyB-QXowCPhJcHnLZTzqijlW0vo-34FL8nw",
+    authDomain: "fitness-tracker-c0bff.firebaseapp.com",
+    projectId: "fitness-tracker-c0bff",
+    storageBucket: "fitness-tracker-c0bff.appspot.com",
+    messagingSenderId: "12106479554",
+    appId: "1:12106479554:web:40838d10bcadadc67ec85b",
+  },
 };
